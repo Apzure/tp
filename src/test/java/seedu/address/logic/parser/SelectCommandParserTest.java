3effa28ffb4dac3f01207a406c042b4f2e03d40d
Apzure <block4556@gmail.com>
2024-03-16 01:12:55 +0800

    Add Testing

---
+++ src/test/java/seedu/address/logic/parser/SelectCommandParserTest.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+public class SelectCommandParserTest {
+    private SelectCommandParser selectCommandParser= new SelectCommandParser();
+
+    @Test
+    public void parse_success() {
+
+    }
+
+    @Test
+    public void parse_failure() throws ParseException {
+
+    }
+}
